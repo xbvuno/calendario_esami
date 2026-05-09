ef48c9baecefebb993e1172df252fe982f6a73c1
xbvuno <cerrabruno.ist@gmail.com>
2026-05-09 17:56:49 +0200

    fix: added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/calendario_esami/',
+
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    babel({
+      presets: [reactCompilerPreset()]
+    })
+  ]
 })
